20ab3cd7de4dfd685f06585e1cd7f0aeddd1f85a
hagoshaile <hagosb.haile@gmail.com>
2024-07-28 14:41:57 -0500

    updated main app class for consumer side

---
+++ kafka-consumer-db/src/main/java/com/wikimedia/kafka/kafkaConsumerDbApplication.java
@@ -0,0 +1,12 @@
+package com.wikimedia.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class kafkaConsumerDbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(kafkaConsumerDbApplication.class, args);
+    }
+}
